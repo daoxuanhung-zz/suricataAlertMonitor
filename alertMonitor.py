51c64bdf71fdf8d408630b5b08f82d11161f9659
daoxuanhung <42017131+daoxuanhung@users.noreply.github.com>
2018-08-07 13:43:27 +0700

    convert tabs to spaces

--- alertMonitor.py
+++ alertMonitor.py
@@ -18,37 +18,37 @@ line = int(config['alert']['ProcessLine'])
 reader = geolite2.reader()
 
 while(True):
-	alert = data.getAlert(line)
+    alert = data.getAlert(line)
 
-	if (len(alert) > 0):
-		ipInfo = abuse.getSpamStatus(alert['ip_src'])
+    if (len(alert) > 0):
+        ipInfo = abuse.getSpamStatus(alert['ip_src'])
 
-		print (Fore.YELLOW + alert['sensor'] + " - " + alert['timestamp'] + '\t' + alert['ip_src'] + '\t' + alert['signature'] + Style.RESET_ALL)
-		if (ipInfo['reportnumber'] > 0):
-			print (Fore.RED + '\tMarked as abuse with ' + Fore.GREEN + str(ipInfo['reportnumber']) + Fore.RED + ' times in last 30 days.')
-			print ('\tAbuse category: ' + ipInfo['cate_str'] + Style.RESET_ALL)
+        print (Fore.YELLOW + alert['sensor'] + " - " + alert['timestamp'] + '\t' + alert['ip_src'] + '\t' + alert['signature'] + Style.RESET_ALL)
+        if (ipInfo['reportnumber'] > 0):
+            print (Fore.RED + '\tMarked as abuse with ' + Fore.GREEN + str(ipInfo['reportnumber']) + Fore.RED + ' times in last 30 days.')
+            print ('\tAbuse category: ' + ipInfo['cate_str'] + Style.RESET_ALL)
 
-			if (ipInfo['reportnumber'] >= 10):
-				with open('badip.txt', 'a') as f:
-					f.write(alert['ip_src'] + '\n')
+            if (ipInfo['reportnumber'] >= 10):
+                with open('badip.txt', 'a') as f:
+                    f.write(alert['ip_src'] + '\n')
 
-		else:
-			print (Fore.GREEN + '\tNot marked as abuse' + Style.RESET_ALL)
+        else:
+            print (Fore.GREEN + '\tNot marked as abuse' + Style.RESET_ALL)
 
-		location = reader.get(alert['ip_src'])
-		try:
-			print ('\tCountry: ' + location['country']['names']['en'] + ' - ' + location['city']['names']['en'])
-		except Exception as e:
-			try:
-				print ('\tCountry: ' + location['country']['names']['en'])
-			except Exception as e:
-				print ('\tCannot get country name')
-			
+        location = reader.get(alert['ip_src'])
+        try:
+            print ('\tCountry: ' + location['country']['names']['en'] + ' - ' + location['city']['names']['en'])
+        except Exception as e:
+            try:
+                print ('\tCountry: ' + location['country']['names']['en'])
+            except Exception as e:
+                print ('\tCannot get country name')
+            
 
-		print ()
-		line += 1
-	else:
-		time.sleep(1)
+        print ()
+        line += 1
+    else:
+        time.sleep(1)
 
-	config['alert']['ProcessLine'] = str(line)
-	saveConfig(config)
+    config['alert']['ProcessLine'] = str(line)
+    saveConfig(config)
--- init.py
+++ init.py
@@ -5,62 +5,62 @@ import configparser
 import os
 
 def unique(list1):
-	# insert the list to the set
-	list_set = set(list1)
-	# convert the set to the list
-	return list(list_set)
+    # insert the list to the set
+    list_set = set(list1)
+    # convert the set to the list
+    return list(list_set)
 
 def readConfig():
-	configreader = configparser.ConfigParser()
-	configreader.read('config.ini')
-	return configreader
+    configreader = configparser.ConfigParser()
+    configreader.read('config.ini')
+    return configreader
 
 def writeConfig():
-	configwriter = configparser.ConfigParser()
+    configwriter = configparser.ConfigParser()
 
-	configwriter['mysql'] = { 	'ServerAddress': '127.0.0.1', 
-								'User': 'suricata',
-								'Password': 'password',
-								'Database': 'suricata'}	
-	configwriter['alert'] = {	'ProcessLine': '0'	}
-	configwriter['abusedb'] = {	'ApiKey': 'put_your_api_key_here'}
-	saveConfig(configwriter);
+    configwriter['mysql'] = {   'ServerAddress': '127.0.0.1', 
+                                'User': 'suricata',
+                                'Password': 'password',
+                                'Database': 'suricata'}    
+    configwriter['alert'] = {   'ProcessLine': '0'    }
+    configwriter['abusedb'] = { 'ApiKey': 'put_your_api_key_here'}
+    saveConfig(configwriter);
 
-	print("Write new config successful. Please edit config.ini file and restart program.")
-	exit()
+    print("Write new config successful. Please edit config.ini file and restart program.")
+    exit()
 
 def saveConfig(newconfig):
-	with open('config.ini', 'w') as configfile:
-		newconfig.write(configfile)
+    with open('config.ini', 'w') as configfile:
+        newconfig.write(configfile)
 
 def verifyConfig():
-	print ("Reading config.")
-	configverify = readConfig()
+    print ("Reading config.")
+    configverify = readConfig()
 
-	if ('mysql' not in configverify):
-		writeConfig()
-	elif ('ServerAddress' not in configverify['mysql']):
-		print("ServerAddress value is empty.")
-	elif ('User' not in configverify['mysql']):
-		print("User value is empty.")
-	elif ('Password' not in configverify['mysql']):
-		print("Password value is empty.")
-	elif ('Database' not in configverify['mysql']):
-		print("Database value is empty.")
-	elif ('alert' not in configverify):
-		writeConfig()
-	elif ('ProcessLine' not in configverify['alert']):
-		print("ProcessLine value is empty.")
-	elif ('abusedb' not in configverify):
-		writeConfig()
-	elif ('ApiKey' not in configverify['abusedb']):
-		print("ApiKey value is empty.")
-	else:
-		print ("Read config success.")
-		return configverify
-	
-	writeConfig() #write default config
-	exit()	# quit if anything wrong
+    if ('mysql' not in configverify):
+        writeConfig()
+    elif ('ServerAddress' not in configverify['mysql']):
+        print("ServerAddress value is empty.")
+    elif ('User' not in configverify['mysql']):
+        print("User value is empty.")
+    elif ('Password' not in configverify['mysql']):
+        print("Password value is empty.")
+    elif ('Database' not in configverify['mysql']):
+        print("Database value is empty.")
+    elif ('alert' not in configverify):
+        writeConfig()
+    elif ('ProcessLine' not in configverify['alert']):
+        print("ProcessLine value is empty.")
+    elif ('abusedb' not in configverify):
+        writeConfig()
+    elif ('ApiKey' not in configverify['abusedb']):
+        print("ApiKey value is empty.")
+    else:
+        print ("Read config success.")
+        return configverify
+    
+    writeConfig() #write default config
+    exit()    # quit if anything wrong
 
 # clear screen
 os.system('cls' if os.name=='nt' else 'clear')
